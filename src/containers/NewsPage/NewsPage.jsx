bb79b17b10a8d51a5d90585956816d6b44cc464c
Nickew <Extr1m7777@gmail.com>
2018-05-10 16:39:39 +0300

    [Redux build #13 - News page useless code removed]

--- src/containers/NewsPage/NewsPage.jsx
+++ src/containers/NewsPage/NewsPage.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { addNews } from '../../actions/addNews';
 import { removeNews } from '../../actions/removeNews';
-import { showCatNews } from '../../actions/showCatNews';
 import MainContainer from '../../components/MainContainer';
 import News from '../../components/News';
 import Item from '../../components/News/Item';
@@ -15,24 +13,7 @@ class NewsPage extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    this.state = { title: '', message: '' };
-    this.addNewsItem = this.addNewsItem.bind(this);
-    this.onUpdateNewsTitle = this.onUpdateNewsTitle.bind(this);
     this.deleteNewsItem = this.deleteNewsItem.bind(this);
-    this.onUpdateNewsMessage = this.onUpdateNewsMessage.bind(this);
-    this.showCategory = this.showCategory.bind(this);
-  }
-
-  onUpdateNewsTitle(e) {
-    this.setState({ title: e.target.value });
-  }
-
-  onUpdateNewsMessage(e) {
-    this.setState({ message: e.target.value });
-  }
-
-  addNewsItem() {
-    this.props.addNewsItem(this.state.title, this.state.message);
   }
 
   deleteNewsItem(e, index) {
@@ -40,17 +21,15 @@ class NewsPage extends React.PureComponent {
     this.props.deleteNewsItem(index);
   }
 
-  showCategory(e, id) {
-    e.preventDefault();
-    this.props.showCategory(id);
-  }
-
   render() {
     const { news, newsCategories } = this.props;
+
     const mappedCats = newsCategories.map((cat) =>
       <li key={cat.id}><Link to={`/news/category/${cat.url}`}>{cat.name}</Link></li>);
+
     const mappedNews = news.map((entity) =>
       <Item id={entity.id} title={entity.title} message={entity.message} key={entity.id} onClick={(e) => this.deleteNewsItem(e, entity.id)} buttonText="Delete" />);
+
     return (
       <MainContainer>
         <News>{mappedNews}</News>
@@ -70,17 +49,13 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = {
-  addNewsItem: addNews,
   deleteNewsItem: removeNews,
-  showCategory: showCatNews,
 };
 
 NewsPage.propTypes = {
   news: PropTypes.array.isRequired,
   newsCategories: PropTypes.array.isRequired,
-  addNewsItem: PropTypes.func.isRequired,
   deleteNewsItem: PropTypes.func.isRequired,
-  showCategory: PropTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(NewsPage);
